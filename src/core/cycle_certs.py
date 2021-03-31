d3bc317a6f4a128047d59c51425c95b38dbac117
nonchris <git@chris-ge.de>
2021-03-31 16:19:33 +0200

    Disable deletion of db entry when domain is unreachable
    This could be re implemented one day with a counter field that counts if the domain is e.g. 7 days unreachable in a row before deletion...

--- src/core/cycle_certs.py
+++ src/core/cycle_certs.py
@@ -51,13 +51,14 @@ def cycle_certs() -> List[Tuple[int, str]]:
             # Remove it from db add waring message and continue
             if not cert:
                 errors += 1
-                message = utl.prep_for_md(f'*ERROR!*\nThere is no certificate for {utl.mk_link(domain.domain, domain.port)}\n'
-                          f'Please check your certificate _immediately_!'
-                          f'_This domain was removed from your watchlist. You can add it again after it got a new cert._',
+                message = utl.prep_for_md(f"*ERROR!*\nCan't resolve {utl.mk_link(domain.domain, domain.port)}\n"
+                          f'Please check your service _immediately_!',
+                          # f'_This domain was removed from your watchlist. You can add it again after it got a new cert._',
                                           ignore=['*', '_'])
                 updates.append((user.chat_id, message))
-                session.query(dbm.Domains).filter(dbm.Domains.domain == domain.domain).delete()
-                session.commit()
+                # TODO: this should not happen immediately...
+                # session.query(dbm.Domains).filter(dbm.Domains.domain == domain.domain).delete()
+                # session.commit()
                 logging.warning(f"{domain.domain}:{domain.port} expired, removed it from database")
                 continue
 
